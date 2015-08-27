2005a07414e9826865361eb53a0a70e56fff19a6
toshiyuki.hasegawa <toshiyuki.hasegawa@torchlight.co.jp>
2015-08-27 13:13:47 +0900

    Fixed to wait for API response.

--- lib/wbench/browser.rb
+++ lib/wbench/browser.rb
@@ -69,8 +69,24 @@ module WBench
 
     def wait_for_page
       Selenium::WebDriver::Wait.new(:timeout => CAPYBARA_TIMEOUT).until do
-        session.evaluate_script('window.performance.timing.loadEventEnd').to_i > 0
+        is_finished_load_event_end = session.evaluate_script('window.performance.timing.loadEventEnd').to_i > 0
+        ( is_finished_load_event_end && is_finished_mark )
       end
+    end
+
+    def is_finished_mark
+        marks = session.evaluate_script('window.performance.getEntriesByType("mark")')
+        start_count = 0
+        finished_count = 0
+        marks.each do |mark|
+          case mark['name']
+          when /^(Start:)/
+            start_count = start_count + 1
+          when /^(Finished:)/
+            finished_count = finished_count + 1
+          end
+        end
+        ( start_count === finished_count )
     end
 
     def set_cookies
